880a8f8459ec8570318a7d5cab9b80b58852d21f
Wu Mianzhi <1427207878@qq.com>
2024-07-01 18:23:56 +0800

    formatted code

--- emotion/trainer.py
+++ emotion/trainer.py
@@ -6,7 +6,7 @@ from torchvision import transforms
 import tqdm
 
 class trainer:
-    def __init__(self, model, dataset, device, batch_size=32, num_epochs=10, lr=0.001, **kwargs):
+    def __init__(self, model, dataset, device, val_dataset = None, metric = None, batch_size=32, num_epochs=10, lr=0.001, **kwargs):
         self.model = model
         self.dataset = dataset
         self.batch_size = batch_size
@@ -17,15 +17,28 @@ class trainer:
         self.model.to(self.device)
 
         self.criterion = nn.CrossEntropyLoss()
-        self.optimizer = optim.Adam(self.model.parameters(), lr=self.lr)
+        self.optimizer = optim.AdamW(self.model.parameters(), lr=self.lr)
+        self.scheduler = optim.lr_scheduler.CosineAnnealingWarmRestarts(self.optimizer, T_0=10)
+        
+        if val_dataset:
+            assert metric, "Metric must be provided if validation dataset is provided"
+            self.val_dataset = val_dataset
+            self.metric = metric
+            self.best_metric = 0.0
+            self.best_model_epoch = -1
+            self.val = True
+        else:
+            self.val = False
 
     def train(self):
         train_loader = DataLoader(self.dataset, batch_size=self.batch_size, shuffle=True)
-
+        print("Starting Training")
         for epoch in range(self.num_epochs):
             self.model.train()
             running_loss = 0.0
+            print(f"Epoch {epoch+1}/{self.num_epochs}")
             for i, data in enumerate(train_loader, 0):
+                # print(f"Batch {i+1}/{len(train_loader)}")
                 inputs, labels = data
                 inputs, labels = inputs.to(self.device), labels.to(self.device)
 
@@ -38,6 +51,15 @@ class trainer:
 
                 running_loss += loss.item()
             print(f"Epoch {epoch+1}, Loss: {running_loss/len(train_loader)}")
+            self.scheduler.step()
+            
+            if self.val:
+                metric = self.metric(self.model, self.val_dataset, self.device, self.batch_size)
+                print(f"Validation Metric: {metric}")
+                if metric > self.best_metric:
+                    self.best_metric = metric
+                    self.best_model_epoch = epoch
+                    torch.save(self.model.state_dict(), "best_model.pth")
 
         print("Finished Training")
         return self.model
--- main.py
+++ main.py
@@ -1,24 +1,68 @@
 import argparse
 import os
+import random
 
+import numpy as np
 import torch
+from torchvision import datasets, transforms
 from emotion.data import EmotionDataset
 from emotion.models.base import BaseModel
 from emotion.trainer import trainer
 from emotion.utils import eval_model
+
+from emotion.models.convnext import get_model
+
+seed = 2001
+torch.manual_seed(seed)
+torch.cuda.manual_seed(seed)
+random.seed(seed)
+np.random.seed(seed)
+
+train_transform = transforms.Compose(
+        [
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomVerticalFlip(),
+            transforms.Grayscale(),
+            transforms.Resize(236),
+            transforms.RandomRotation(degrees=20),
+            transforms.RandomCrop(224),
+            transforms.ToTensor(),
+            transforms.Lambda(lambda x: x.repeat(3, 1, 1)),
+        ]
+    )
+
+test_transform = transforms.Compose(
+        [
+            transforms.Grayscale(),
+            transforms.Resize(224),
+            transforms.ToTensor(),
+            transforms.Lambda(lambda x: x.repeat(3, 1, 1)),
+        ]
+    )
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    # parser.add_argument('--data_dir', type=str, required=True)
-    parser.add_argument('--batch_size', type=int, default=32)
-    parser.add_argument('--num_epochs', type=int, default=10)
-    parser.add_argument('--lr', type=float, default=0.001)
+    parser.add_argument('--batch_size', type=int, default=24)
+    parser.add_argument('--num_epochs', type=int, default=20)
+    parser.add_argument('--lr', type=float, default=1e-4)
+    parser.add_argument('--use_se', action='store_true', default=False)
+    parser.add_argument('--use_sim', action='store_true', default=False)
+    parser.add_argument('--use_stn', action='store_true', default=False)
+    parser.add_argument('--ckpt', type=str, default=None)
     args = parser.parse_args()
     
-    train_dataset = EmotionDataset(root_dir='./dataset/train')
-    test_dataset = EmotionDataset(root_dir='./dataset/test')
+    train_dataset = EmotionDataset(root_dir='./dataset/train', transform=train_transform)
+    # train_dataset, val_dataset = torch.utils.data.random_split(train_dataset, [int(0.8*len(train_dataset)), len(train_dataset)-int(0.8*len(train_dataset))])
+    test_dataset = EmotionDataset(root_dir='./dataset/test', transform=test_transform)
+    # train_dataset = EmotionDataset(root_dir='./dataset/train', transform=None)
+    # test_dataset = EmotionDataset(root_dir='./dataset/test', transform=None)
     # note input figure size is 48x48=2304
-    model = BaseModel(input_dim = 2304, num_classes=7)
+    # model = BaseModel(input_dim = 2304, num_classes=7)
+    model = get_model("convnext_xlarge", num_classes=7, pretrained=True, in_22k=True, use_se=args.use_se, use_sim=args.use_sim, use_stn=args.use_stn)
+    if args.ckpt:
+        print(f"Loading checkpoint from {args.ckpt}")
+        model.load_state_dict(torch.load(args.ckpt))
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    trainer = trainer(model, train_dataset, device, args.batch_size, args.num_epochs, args.lr)
+    trainer = trainer(model, train_dataset, device, test_dataset, eval_model, args.batch_size, args.num_epochs, args.lr)
     model = trainer.train()
     print(f"Accuracy: {eval_model(model, test_dataset, device, args.batch_size)}")
